e68a903dafdd0dc533834051ccc7189b6564782c
William Lemos de Vargas <william.lemosdevargas@gmail.com>
2021-12-04 19:43:03 -0300

    troca de dois ifs por um while

--- desafiobrmed/cotacoes/views.py
+++ desafiobrmed/cotacoes/views.py
@@ -29,12 +29,11 @@ def get_from_db_or_api(date):
 
 # elimina dias não uteis
 def only_work_days(date):
-    if date.weekday() in (5,6):
-        date = date - datetime.timedelta(days=1)
-    if date.weekday() in (5,6):
+    while date.weekday() in (5, 6):
         date = date - datetime.timedelta(days=1)
 
     return date
+
 
 # view
 def home(request, date=datetime.date.today(), columns=5):
@@ -49,6 +48,7 @@ def home(request, date=datetime.date.today(), columns=5):
             columns = form.cleaned_data.get('columns')
 
     while count < columns:
+        date = only_work_days(date)
         date = only_work_days(date)
         taxe = get_from_db_or_api(date)
         taxes.insert(0,taxe)
